5aa97d97d279f7152f07ce8b2081a821bf425f97
sangharsh23 <sangharsh355@gmail.com>
2021-08-29 12:36:47 +0530

    Added: program length of line

--- src/main/java/com/bridgelabz/line/comparision/Line_Comparision.java
+++ src/main/java/com/bridgelabz/line/comparision/Line_Comparision.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.line.comparision;
 
+import java.util.Scanner;
+
 public class Line_Comparision {
     public static void main(String[] args) {
         System.out.println("Welcome to line Comparision Computation Program");
+
+        Scanner line = new Scanner(System.in);
+
+        System.out.println("Enter the Value of x Co-ordinate 1st point: ");
+        int x1 = line.nextInt();
+        System.out.println("Enter the Value of y Co-ordinate  1nd point: ");
+        int y1 = line.nextInt();
+        System.out.println("Enter the Value of x Co-ordinate 2nd point: ");
+        int x2 = line.nextInt();
+        System.out.println("Enter the Value of y Co-ordinate 2nd point: ");
+        int y2 = line.nextInt();
+
+        double length_of_line1 =Math.sqrt((x2 - x1) *(x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("length_of_line1 = " + length_of_line1);
     }
 }
